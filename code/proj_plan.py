dc9d333c38fced90451784beeffe3eb96858c589
Arne Goerlitz <a-goerlitz@hotmail.de>
2018-12-08 20:45:46 +0100

    liitle changes

--- code/proj_plan.py
+++ code/proj_plan.py
@@ -5,16 +5,18 @@ Created on Sun Dec  2 16:21:14 2018
 @author: Goerlitz
 @date: 2.12.2018
 """
+projlist = []
+###############################################################################
+########################define chain-element class#############################
 class Chain_Element:
     def __init__(
-            self, name ='', todo='', istask = True, 
-            ismilestone= False, check = False
+            self, name ='', info='',  
+            ismilestone= False, state = False
         ):
         self.name = name
-        self.todo = todo
-        self.istask = istask
+        self.info = info
         self.ismilestone = ismilestone
-        self.check = check
+        self.state = state
         return
     
     def rename(self,new_name):
@@ -22,14 +24,24 @@ class Chain_Element:
         return
     
     def check(self):
-        self.check = True
+        self.state = True
         return
     
+###############################################################################   
+#########################define proj objekt####################################
 class Proj:
-    def __init__(self, name=''):
+    def __init__(self, name='', current_task_index = 0):
         self.name = name
         self.tasklist = []
-        self.current_task = 0
+        #Set a Start-Milestone
+        self.tasklist.append(
+            Chain_Element(
+                'start: '+self.name,
+                ismilestone = True,
+                state = True
+            )
+        )
+        self.current_task_index = current_task_index
         return
     
     def rename(self,new_name):
@@ -37,32 +49,91 @@ class Proj:
         return 
     
     def insert_element(self, element, pos=-1):
-        self.tasklist.insert(pos, element)
+        if pos == -1:
+            self.tasklist.append(element)
+            if self.tasklist[pos-1].state == True:
+                self.current_task_index += 1
+        else:
+            self.tasklist.insert(pos, element)
+            #If state of last task is True(solved) set itertater on new task
+            if self.tasklist[pos-1].state ==True and self.tasklist[pos].state==False:
+                self.current_task_index =pos
+            elif self.tasklist[pos-1].state ==True and self.tasklist[pos].state==True:
+                self.current_task_index += 1
         return
     
-    def proj_task_soved(self, pos):
+    def del_element(self,pos=-1):
+        del self.tasklist[pos]
+        return
+    
+    def task_solved(self, pos):
         task = self.tasklist[pos]
-        
+        print(self.name+': '+task.name+' solved')
         task.check()
         
-        if pos == self.current_task:
-            self.current_task += 1
+        if pos == self.current_task_index and pos == len(self.tasklist)-1:
+            self.current_task_index += 1
         return
-def main():    
-    startup = Proj ('Startup')
-    start = Chain_Element('Start des Projekts', 'Um Fortzufahren muss man folgendes tun')
-    startup.insert_element(start)
+###############################################################################
+###########################Global Functions####################################        
+
+def update_indecies(projlist):   
+    for proj in projlist:
+        proj.current_task_index = 0
+        while proj.tasklist[proj.current_task_index].state != False:
+            if proj.tasklist[len(proj.tasklist)-1].state == True:
+                proj.current_task_index = len(proj.tasklist)-1
+                break
+            proj.current_task_index += 1
+    return
+ 
+def add_projekt(proj_name):
+    projlist.append(Proj(proj_name))
+    return
+
+def del_projekt(num):
+    del projlist[num]
     
-    print(startup.name)
-    print(startup.tasklist[0].todo)
-    return 
+    return
+
+def add_task(proj,element_name, info='', ismilestone= False, state = False):
+    element = Chain_Element (element_name, info, ismilestone, state)
+    proj.insert_element(element)
+    return
+
+def del_task(proj,pos):
+    proj.del_element(pos)
+    return
+###############################################################################
+#################################Testing Aera##################################
 
 
+def main():  
+    add_projekt('Startup')
+    add_task(projlist[0],'Erster Task')
+    add_task(projlist[0],'Zweiter Task','')
+    add_task(projlist[0],'Dritter Task')
+    #startup = Proj('Startup')
+    #start = Chain_Element('Erster Task', 'Um Fortzufahren muss man folgendes tun')
+    #zweiter_task = Chain_Element('Zwei')
+    #startup.insert_element(start) 
+    #startup.insert_element(zweiter_task)
+    #projlist.append(startup)
+    #update_indecies(projlist)    
+        
 
+    print(projlist[0].name)
+    print(projlist[0].tasklist[0].name)
+    print(projlist[0].tasklist[1].name)
+    print(projlist[0].current_task_index)    
+    
+    projlist[0].task_solved(1)
+    #del_projekt(0)
+    update_indecies(projlist)
+    
+    print(projlist[0].current_task_index)
+    print('Anzahl der Elemente:',len(projlist[0].tasklist))
 
 
 if __name__ == "__main__":
-    main()    
-        
-
-    
+    main()
